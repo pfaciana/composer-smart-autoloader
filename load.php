7b8b69ca39ae364b392d30ccfe405b0be1559b4d
Phil Faciana <pfaciana@gmail.com>
2024-11-11 05:50:43 -0500

    feat: add autoloading for when in use as a WordPress plugin

---
+++ load.php
@@ -0,0 +1,11 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+if ( isset( $GLOBALS['wp_version'] ) && function_exists( 'add_action' ) && basename( $dir = dirname( __DIR__ ) ) !== 'pfaciana' && basename( dirname( $dir ) ) !== 'vendor' ) {
+	\add_action( 'rpuc/init', fn() => new PackageUpgrader\V1\Plugin );
+
+	\add_action( 'plugins_loaded', function () {
+		\Render\Autoload\ClassLoader::getInstance();
+	}, PHP_INT_MIN );
+}
