bb78d0bab3d7614295a03c817121c00f52166a13
Phil Faciana <pfaciana@gmail.com>
2025-07-17 19:06:07 -0400

    fix: gracefully exit script
    - because this file is auto-loaded hard exit can break development scripts

--- load.php
+++ load.php
@@ -1,6 +1,8 @@
 <?php
 
-defined( 'ABSPATH' ) || exit;
+if ( !defined( 'ABSPATH' ) ) {
+	return;
+}
 
 if ( isset( $GLOBALS['wp_version'] ) && function_exists( 'add_action' ) && basename( $dir = dirname( __DIR__ ) ) !== 'pfaciana' && basename( dirname( $dir ) ) !== 'vendor' ) {
 	\add_action( 'plugins_loaded', function () {
